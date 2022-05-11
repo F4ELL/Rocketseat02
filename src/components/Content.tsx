b90ef2204282f28e2645965992f4265e08c3f6b0
F4ELL <ferreiraperes1234@gmail.com>
2022-05-11 19:50:10 -0300

    feat: create content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
+import React from 'react'
+import { GenreResponseProps, MovieProps } from '../App'
+import { MovieCard } from './MovieCard'
+
+interface TContentProps {
+  movies: MovieProps[],
+  selectedGenre: GenreResponseProps
+}
+
+export function Content({ movies, selectedGenre }: TContentProps) {
   // Complete aqui
+  return(
+    <>
+      <div className="container">
+          <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          </header>
+          <main>
+            <div className="movies-list">
+              {movies.map(movie => (
+                <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              ))}
+            </div>
+          </main>
+      </div>
+    </>
+  )
 }
