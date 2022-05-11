3ee54c155bb6efc7a9f37402e0684bcb968e8952
F4ELL <ferreiraperes1234@gmail.com>
2022-05-11 19:49:56 -0300

    feat: create sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
+import React from 'react'
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface TSideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  setSelectedGenreId: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: TSideBarProps) {
   // Complete aqui
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return(
+      <>
+        <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+        </nav>
+      </>
+  )
 }
